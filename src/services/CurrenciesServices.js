4359599813d30e93bbc2ff87e51982ad5220dbd9
ji.ccdev <ji.ccdev@outlook.com>
2022-06-23 09:42:57 -0400

    feat: after-sales-module

--- src/services/CurrenciesServices.js
+++ src/services/CurrenciesServices.js
@@ -1,13 +1,13 @@
 import api from "../config/api";
 const token = localStorage.getItem("access_token");
 
-const ValidDealsServices = {
-  getValidDeals: async () => {
-    const response = await api.get(`/deals`, {
+const CurrenciesServices = {
+  getCurrency: async () => {
+    const response = await api.get(`/currencies`, {
       headers: { Authorization: `Bearer ${token}` },
     });
     return response.data;
   },
 };
 
-export default ValidDealsServices;
+export default CurrenciesServices;
